2f5009e10434f7c48e2211a4785bb0abce02021a
Warit Santaputra <waritsan@gmail.com>
2020-08-10 16:31:40 +0700

    Add docker compose

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
-        res.send('Number of visits is' + visits);
+        res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
 });
